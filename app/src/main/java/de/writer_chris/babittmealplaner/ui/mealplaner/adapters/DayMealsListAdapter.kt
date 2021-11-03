842024a3ba4c43760a978e1bc157a8e8978394ee
Christopher Becker <toffy@freundederinteraktion.de>
2021-11-03 15:40:35 +0100

    enhanced adapter

--- app/src/main/java/de/writer_chris/babittmealplaner/ui/mealplaner/adapters/DayMealsListAdapter.kt
+++ app/src/main/java/de/writer_chris/babittmealplaner/ui/mealplaner/adapters/DayMealsListAdapter.kt
@@ -1,19 +1,19 @@
 package de.writer_chris.babittmealplaner.ui.mealplaner.adapters
 
-import android.icu.text.SimpleDateFormat
-import android.icu.util.Calendar
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import de.writer_chris.babittmealplaner.R
+import de.writer_chris.babittmealplaner.data.entities.relations.MealAndDish
+import de.writer_chris.babittmealplaner.data.utility.CalendarUtil
 import de.writer_chris.babittmealplaner.databinding.ItemMealDayBinding
-
 import de.writer_chris.babittmealplaner.ui.mealplaner.models.DayMealsAndDish
 
 
 class DayMealsListAdapter(
+    private val context: Context,
     private val onItemSelect: (mealId: Int) -> Unit,
     private val onItemRead: (dishMealId: Array<Int>) -> Unit
 ) :
@@ -44,81 +44,37 @@ class DayMealsListAdapter(
         RecyclerView.ViewHolder(binding.root) {
         fun bind(
             dayMeals: DayMealsAndDish,
+            context: Context,
             onItemSelect: (mealId: Int) -> Unit,
             onItemRead: (dishMealId: Array<Int>) -> Unit
         ) {
-
-            //TODO make it more nice!!
-            val sdf = SimpleDateFormat("EEE - dd.MM.yyyy")
-            val cal = Calendar.getInstance()
-            cal.timeInMillis = dayMeals.date
             binding.apply {
-                txtDate.text = sdf.format(cal).toString()
-
-                if (dayMeals.breakfast.meal.dishId == null) {
-                    txtBreakfastDish.text = "-"
-                    imgbtnBreakfast.setImageResource(R.drawable.ic_breakfast)
-                    imgbtnBreakfast.setOnClickListener {
-                        onItemSelect(dayMeals.breakfast.meal.mealId)
-                    }
-
-                } else {
-                    txtBreakfastDish.text = dayMeals.breakfast.dish?.dishName
-                    //TODO change when Dish image has resource
-                    imgbtnBreakfast.setImageResource(R.drawable.ic_breakfast)
-                    imgbtnBreakfast.setOnClickListener {
-                        onItemRead(
-                            arrayOf(
-                                dayMeals.breakfast.dish?.dishId ?: -1,
-                                dayMeals.breakfast.meal.mealId
-                            )
-                        )
-                    }
-                }
-                if (dayMeals.lunch.meal.dishId == null) {
-                    txtLunchDish.text = "-"
-                    imgbtnLunch.setOnClickListener {
-                        onItemSelect(dayMeals.lunch.meal.mealId)
-                    }
-                    imgbtnLunch.setImageResource(R.drawable.ic_lunch)
-
-                } else {
-                    txtLunchDish.text = dayMeals.lunch.dish?.dishName
-                    //TODO change when Dish image has resource
-                    imgbtnLunch.setImageResource(R.drawable.ic_lunch)
-                    imgbtnLunch.setOnClickListener {
-                        onItemRead(
-                            arrayOf(
-                                dayMeals.lunch.dish?.dishId ?: -1,
-                                dayMeals.lunch.meal.mealId
-                            )
-                        )
-                    }
-                }
-
-                if (dayMeals.dinner.meal.dishId == null) {
-                    txtDinnerDish.text = "-"
-                    imgbtnDinner.setImageResource(R.drawable.ic_dinner)
-                    imgbtnDinner.setOnClickListener {
-                        onItemSelect(dayMeals.dinner.meal.mealId)
-                    }
-
-                } else {
-                    txtDinnerDish.text = dayMeals.dinner.dish?.dishName
-                    //TODO change when Dish image has resource
-                    imgbtnDinner.setImageResource(R.drawable.ic_dinner)
-                    imgbtnDinner.setOnClickListener {
-                        onItemRead(
-                            arrayOf(
-                                dayMeals.dinner.dish?.dishId ?: -1,
-                                dayMeals.dinner.meal.mealId
-                            )
-                        )
-                    }
-                }
-
-
+                txtDate.text = getDate(dayMeals.date)
+                txtDayOfTheWeek.text = getDayOfWeek(dayMeals.date)
+                recyclerViewDayMeals.adapter =
+                    getAdapter(dayMeals, context, onItemSelect, onItemRead)
             }
+        }
+
+        private fun getDate(date: Long): String {
+            return CalendarUtil.longToGermanDate(date)
+        }
+
+        private fun getDayOfWeek(date: Long): String {
+            return CalendarUtil.longToWeekday(date)
+        }
+
+        private fun getAdapter(
+            dayMeals: DayMealsAndDish,
+            context: Context,
+            onItemSelect: (mealId: Int) -> Unit,
+            onItemRead: (dishMealId: Array<Int>) -> Unit
+        ): DayMealItemListAdapter {
+            val itemList: List<MealAndDish> =
+                listOf(dayMeals.breakfast, dayMeals.lunch, dayMeals.dinner)
+            val adapter = DayMealItemListAdapter(context, onItemSelect, onItemRead)
+            adapter.submitList(itemList)
+            return adapter
         }
     }
 
@@ -132,16 +88,11 @@ class DayMealsListAdapter(
                     parent.context
                 ), parent, false
             )
-
-
         )
     }
 
     override fun onBindViewHolder(holder: MealViewHolder, position: Int) {
         val current = getItem(position)
-        holder.bind(current, onItemSelect, onItemRead)
-
+        holder.bind(current, context, onItemSelect, onItemRead)
     }
-
-
 }
