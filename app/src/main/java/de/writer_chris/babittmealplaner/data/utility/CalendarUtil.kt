b3a3e7547f5c1385984ab3e0b8a787294cb88758
Christopher Becker <toffy@freundederinteraktion.de>
2021-11-03 15:35:36 +0100

    enhanced CalendarUtil.kt

--- app/src/main/java/de/writer_chris/babittmealplaner/data/utility/CalendarUtil.kt
+++ app/src/main/java/de/writer_chris/babittmealplaner/data/utility/CalendarUtil.kt
@@ -1,5 +1,6 @@
 package de.writer_chris.babittmealplaner.data.utility
 
+import android.annotation.SuppressLint
 import android.icu.text.SimpleDateFormat
 import android.util.Log
 import de.writer_chris.babittmealplaner.R
@@ -9,47 +10,28 @@ import android.icu.util.Calendar
 
 class CalendarUtil {
     companion object {
-        fun localDateToCalendar(localDate: LocalDate): Calendar {
-            val cal = Calendar.getInstance()
-            cal.set(localDate.year, (localDate.monthValue - 1), localDate.dayOfMonth, 0, 0, 0)
-            val sdf = SimpleDateFormat("EEE yyyy-MM-dd ")
-//            Log.d("Date", "${sdf.format(cal.time)}")
 
-            return cal
-        }
 
-        fun calendarToLong(cal: Calendar): Long {
-            return cal.timeInMillis
-        }
-
-        fun longToCalendar(number: Long): Calendar {
-            val cal = Calendar.getInstance()
-            cal.timeInMillis = number
-            return cal
-        }
-
-        fun longToWeekdayResId(number: Long): Int {
+        fun longToWeekday(number: Long): String {
             val cal = longToCalendar(number)
-            return calendarToWeekdayResId(cal)
-        }
-
-        fun calendarToWeekdayResId(cal: Calendar):Int{
-            val weekday: Int = when (cal.get(Calendar.DAY_OF_WEEK)) {
-                2 -> R.string.monday
-                3 -> R.string.tuesday
-                4 -> R.string.wednesday
-                5 -> R.string.thursday
-                6 -> R.string.friday
-                7 -> R.string.saturday
-                else -> R.string.sunday
-            }
-            return weekday
+            return calendarToWeekday(cal)
         }
 
         fun longToGermanDate(number: Long): String {
             val sdf = SimpleDateFormat("dd.MM.yyyy")
-            return sdf.format(longToCalendar(number).time)
+            return sdf.format(longToCalendar(number)).toString()
         }
 
+        fun calendarToWeekday(cal: Calendar): String {
+            val sdf = SimpleDateFormat("EEEE")
+            return sdf.format(cal).toString()
+        }
+
+
+        private fun longToCalendar(number: Long): Calendar {
+            val cal = Calendar.getInstance()
+            cal.timeInMillis = number
+            return cal
+        }
     }
 }
