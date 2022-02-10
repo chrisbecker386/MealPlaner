1a857ca084172e7a49593d35845ec44817f5e320
Christopher Becker <toffy@freundederinteraktion.de>
2022-02-10 10:57:49 +0100

    removed dead code

--- app/src/main/java/de/writer_chris/babittmealplaner/data/utility/Permissions.kt
+++ app/src/main/java/de/writer_chris/babittmealplaner/data/utility/Permissions.kt
@@ -5,28 +5,15 @@ import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
-import android.util.Log
-import androidx.activity.result.ActivityResultLauncher
-import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import java.security.Permission
 
 class Permissions {
-
     companion object {
 
-        fun hasWritePermission(context: Context): Boolean {
-            return ContextCompat.checkSelfPermission(
-                context, Manifest.permission.WRITE_EXTERNAL_STORAGE
-            ) == PackageManager.PERMISSION_GRANTED || Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
-        }
-
-        fun hasReadPermission(context: Context): Boolean {
-            return ContextCompat.checkSelfPermission(
-                context, Manifest.permission.READ_EXTERNAL_STORAGE
-            ) == PackageManager.PERMISSION_GRANTED
-        }
+        fun hasWritePermission(context: Context) = ContextCompat.checkSelfPermission(
+            context, Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED || Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
 
         fun permissionRequestWriteExternal(context: Context) {
             val permissionsToRequest = mutableListOf<String>()
@@ -41,20 +28,5 @@ class Permissions {
                 )
             }
         }
-
-        fun permissionRequestReadExternal(context: Context) {
-            val permissionsToRequest = mutableListOf<String>()
-            if (!hasReadPermission(context)) {
-                permissionsToRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE)
-            }
-            if (permissionsToRequest.isNotEmpty()) {
-                ActivityCompat.requestPermissions(
-                    context as Activity,
-                    permissionsToRequest.toTypedArray(),
-                    PermissionCode.READ_EXTERNAL.requestCode
-                )
-            }
-        }
-
     }
 }
