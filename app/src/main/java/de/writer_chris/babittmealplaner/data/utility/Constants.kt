8790fc3c7e178fc214ae2ca81f0ff46a561d0093
Christopher Becker <toffy@freundederinteraktion.de>
2022-02-10 10:21:37 +0100

    removed dead code

--- app/src/main/java/de/writer_chris/babittmealplaner/data/utility/Constants.kt
+++ app/src/main/java/de/writer_chris/babittmealplaner/data/utility/Constants.kt
@@ -31,10 +31,6 @@ enum class PermissionCode(val requestCode: Int) {
     LOCATION_BACKGROUND(2)
 }
 
-enum class StorageAccessFrameworkCode(val requestCode: Int) {
-    OPEN_REQUEST_CODE(41)
-}
-
 enum class ErrorMessage(val resId: Int) {
     ERROR_PDF_SAVE(R.string.dialog_save_error_occured),
     ERROR_PDF_SHARE(R.string.dialog_share_error_occured)
@@ -48,7 +44,6 @@ enum class SharePrefState(
     APP_START_REGULAR(key = "isAppAlreadyRun", value = true)
 }
 
-
 const val MINIMAL_DAY = 1
 const val MAXIMAL_DAY = 8
 const val PIXA_BAY_BASE_URL = "https://pixabay.com/"
@@ -57,26 +52,31 @@ const val NOTIFICATION_ID = 0
 
 val DISH_LIST_STARTUP = listOf(
     Dish(
-        1, "Spaghetti Bolognese", 25, "1 Zwiebel\n" +
-                "1 Zehe Knoblauch\n" +
-                "1 Möhre\n" +
-                "500 g Hackfleisch (Rinderhack oder Tartar)\n" +
-                "Salz und Pfeffer\n" +
-                "200 ml Gemüsebrühe (Instant)\n" +
-                "70 g Tomatenmark\n" +
-                "1 TL Oregano\n" +
-                "400 g Tomate(n), gestückelt, mit Kräutern, z.B. aus dem Tetrapack\n" +
-                "2 EL Tomatenketchup\n" +
-                "500 g Nudeln (Spaghetti)\n\n" +
-                "Zwiebel, Knoblauch und Möhre schälen und in feine Würfel schneiden. Hackfleisch in die Pfanne geben, langsam erhitzen und im eigenen Fett unter Rühren anbraten. Salzen und pfeffern. Zwiebeln, Knoblauch und Möhren dazugeben und kurz mitbraten. Mit der Brühe ablöschen, Tomatenmark, Oregano, die gestückelten Tomaten und Tomatenketchup unterrühren. Etwa 40 Minuten einkochen lassen.\n" +
-                "Spaghetti in Salzwasser bissfest kochen, abgießen, abschrecken und zusammen mit der Sauce servieren.\n" +
-                "Source: https://www.chefkoch.de/rezepte/393031127655461/Spaghetti-Bolognese.html"
+        1, "Spaghetti Bolognese", 25, """
+1 Zwiebel
+1 Zehe Knoblauch
+1 Möhre
+500 g Hackfleisch (Rinderhack oder Tartar)
+Salz und Pfeffer
+200 ml Gemüsebrühe (Instant)
+70 g Tomatenmark
+1 TL Oregano
+400 g Tomate(n), gestückelt, mit Kräutern, z.B. aus dem Tetrapack
+2 EL Tomatenketchup
+500 g Nudeln (Spaghetti)
+Zwiebel, Knoblauch und Möhre schälen und in feine Würfel schneiden. Hackfleisch in die Pfanne geben, langsam erhitzen und im eigenen Fett unter Rühren anbraten. Salzen und pfeffern. Zwiebeln, Knoblauch und Möhren dazugeben und kurz mitbraten. Mit der Brühe ablöschen, Tomatenmark, Oregano, die gestückelten Tomaten und Tomatenketchup unterrühren. Etwa 40 Minuten einkochen lassen.
+Spaghetti in Salzwasser bissfest kochen, abgießen, abschrecken und zusammen mit der Sauce servieren.
+Source: https://www.chefkoch.de/rezepte/393031127655461/Spaghetti-Bolognese.html
+""".trimIndent()
     ),
+
     Dish(
-        2,
-        "Pfannenkuchen",
-        25,
-        "1 Cup Tasse\n1 Ei\n1 Tasse Milch\n\nAlle Zutaten ordentlich verrühren. Der Pfanne entsprechend dosiert in einer Pfanne  mit etwas Butter anbraten und wenden"
+        2, "Pfannenkuchen", 25, """
+1 Tasse Mehl
+1 Ei
+1 Tasse Milch
+Alle Zutaten ordentlich verrühren. Der Pfanne entsprechend dosiert in einer Pfanne  mit etwas Butter anbraten und wenden
+""".trimIndent()
     )
 )
 
